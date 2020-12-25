cc3f4b34f7d234f3d97d7f2fc2b138ea970b4e7e
Paperpotato <cabbage.leaves@gmail.com>
2020-12-25 19:08:15 +0800

    Update summaries
    added preliminary questions, information and answers

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,28 @@
 <script>
-	export let name;
+	// export let name;
+	// import './button.scss';
+  	// import Button, {Group, GroupItem, Label, Icon} from '@smui/button';
+	// export let name;
+	function start() {
+		alert('clicked')
+	}
+	let clicked = 0
 </script>
 
 <main>
-	<h1>Hello meow meow meow {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Welcome to yet another Implicit Association Test!</h1>
+	<p>The goal of this test is to reveal our inherent gender biases in accordance how we categorise words.</p>
+	<p>For more information on this and other tests, visit <a href='https://implicit.harvard.edu/implicit/iatdetails.html'>this website!</a></p>
+
+	<!-- <h1>Welcome!</h1> -->
+	<p>In this study, you will be asked to sort words into groups as fast as you can. There will also be some questions examining your beliefs and some standard demographic questions. This should take about 10 minutes for you to complete, to which you will receive a result and an interpretation.</p>	
+	<p>The task will assess your automatic associations of words with male and female genders.</p>
+	<p>Thanks for trying this out!</p>
+
+	<button on:click={start}>
+		Let's Go!
+	</button>
+	<!-- <Button on:click={() => clicked++} variant="raised"><Label>Let's Go (raised)</Label></Button> -->
 </main>
 
 <style>
---
+++ src/Welcome.svelte
@@ -0,0 +1,71 @@
+<script>
+	import './button.scss';
+  	import Button, {Group, GroupItem, Label, Icon} from '@smui/button';
+	// export let name;
+	function start() {
+		alert('clicked')
+	}
+	let clicked = 0
+	let hotColdAnswers = ['Very warm', 'Moderately warm', 'Slightly warm', 'Neither warm nor cold', 'Slightly cold', 'Moderately cold', 'Very cold']
+	let agreeAnswers = ['Strongly agree', 'Moderately agree', 'Slightly agree', 'Neither agree or disagree', 'Slightly disagree', 'Moderately disagree', 'Strongly disagree']
+	let trueAnswers = ['Totally true for me', 'Mostly true for me', 'Moderately true for me', 'Somewhat true for me', 'Not true at all for me']
+	let genderAnswers = ['Men', 'Women', 'Non-binary', 'None']
+</script>
+
+<main>
+	<h1>Qustionnaire</h1>
+	<!--Questions on beliefs-->
+	<p>How warm or cold do you feel towards men?</p>	
+	<p>How warm or cold do you feel towards women?</p>
+	<p>Men are discriminated against</p>
+	<p>women are discriminated against</p>
+	<p>I prefer the company of men over women</p>
+	<p>I prefer the company of women over men</p>
+	<p>I have a sexual preference for</p>
+	<p>I have a romantic preference for</p>
+	<!--Questions on demographics-->
+	<p>What was your sex assigned at birth?</p>
+	<p>What is your current gender identity</p>
+	<p>Birth year (use date picker)</p>
+	<p>What is your race?</p>
+	<p>What is your ethnicity?</p>
+	<p>How many Implicit Association Tests (IAT) have you performed?</p>
+	<p>What is your political identity?</p>
+	<p>What is your religious affiliation?</p>
+	<p>How religious do you consider yourself to be?</p>
+	<p>What is your country of birth?</p>
+	<p>What is your country of residence?</p>
+	<p>Please indicate what is the highest level of education that you have received</p>
+	<p>What is your occupation?</p>
+
+	<!--4 categories: Male, Female, Good, Bad-->
+
+
+	<button on:click={start}>
+		Let's Go!
+	</button>
+	<Button on:click={() => clicked++} variant="raised"><Label>Let's Go (raised)</Label></Button>
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
